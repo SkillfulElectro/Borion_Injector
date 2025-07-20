6bc6cfafc4030d16419c67f8ebae3db9e3e574a3
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-07-20 15:42:23 +0330

    OpenProcess flags added

--- HorionInjector/Injector.cs
+++ HorionInjector/Injector.cs
@@ -49,6 +49,34 @@ namespace HorionInjector
         public static extern bool SetForegroundWindow(IntPtr hWnd);
         //
 
+        private const uint DELETE                 = 0x00010000;
+        private const uint READ_CONTROL           = 0x00020000;
+        private const uint WRITE_DAC              = 0x00040000;
+        private const uint WRITE_OWNER            = 0x00080000;
+        private const uint SYNCHRONIZE            = 0x00100000;
+        private const uint PROCESS_CREATE_THREAD  = 0x0002;
+        private const uint PROCESS_QUERY_INFORMATION = 0x0400;
+        private const uint PROCESS_VM_OPERATION   = 0x0008;
+        private const uint PROCESS_VM_READ        = 0x0010;
+        private const uint PROCESS_VM_WRITE       = 0x0020;
+        private const uint PROCESS_DUP_HANDLE     = 0x0040;
+        private const uint PROCESS_CREATE_PROCESS = 0x0080;
+        private const uint PROCESS_SET_QUOTA      = 0x0100;
+        private const uint PROCESS_SET_INFORMATION = 0x0200;
+        private const uint PROCESS_SUSPEND_RESUME = 0x0800;
+        private const uint PROCESS_QUERY_LIMITED_INFORMATION = 0x1000;
+        private const uint END                    = 0x0000FFFF;
+
+        private const uint PROCESS_ALL_ACCESS =
+            DELETE | READ_CONTROL | WRITE_DAC | WRITE_OWNER |
+            SYNCHRONIZE |
+            PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION |
+            PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE |
+            PROCESS_DUP_HANDLE | PROCESS_CREATE_PROCESS |
+            PROCESS_SET_QUOTA | PROCESS_SET_INFORMATION |
+            PROCESS_SUSPEND_RESUME | PROCESS_QUERY_LIMITED_INFORMATION |
+            END;
+
         private void Inject(string path)
         {
             if (!File.Exists(path))
@@ -117,7 +145,7 @@ namespace HorionInjector
             }
 
             SetStatus("injecting into " + process.Id);
-            IntPtr handle = OpenProcess((IntPtr)2035711, false, (uint)process.Id);
+            IntPtr handle = OpenProcess(PROCESS_ALL_ACCESS, false, (uint)process.Id);
             if (handle == IntPtr.Zero || !process.Responding)
             {
                 MessageBox.Show("Failed to get process handle");
